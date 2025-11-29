7fa89ced3c5db7cf82f26ab991b5c5a14abfaf48
gaki2 <qudrkr0815@gmail.com>
2025-11-29 22:14:21 +0900

    refacotr: throw type error when new LLMProviderNameSchema added.

--- apps/web/src/app/components/helper/registerProvider.ts
+++ apps/web/src/app/components/helper/registerProvider.ts
@@ -1,23 +1,31 @@
+import { match } from 'ts-pattern';
 import type z from 'zod';
+import {
+  type LLMProviderName,
+  LLMProviderNameSchema,
+} from '@/core/provider/all-models';
 import { GoogleLLMProvider } from '@/core/provider/GoogleLLMProvider';
 import { OpenAILLMProvider } from '@/core/provider/OpenAILLMProvider';
 import { providerRegistry } from '@/core/provider/providerRegistry';
 import type { ConfigSchema } from '@/idb/db';
 
 export const registerProvider = (config: z.infer<typeof ConfigSchema>) => {
-  const { googleApiKey, openaiApiKey } = config;
+  const { apiKeys } = config;
 
-  if (googleApiKey && !providerRegistry.has('google')) {
-    providerRegistry.register(
-      'google',
-      new GoogleLLMProvider({ apiKey: googleApiKey }),
-    );
-  }
+  LLMProviderNameSchema.options.forEach((providerName: LLMProviderName) => {
+    const apiKey = apiKeys[providerName];
 
-  if (openaiApiKey && !providerRegistry.has('openai')) {
-    providerRegistry.register(
-      'openai',
-      new OpenAILLMProvider({ apiKey: openaiApiKey }),
-    );
-  }
+    if (!apiKey || providerRegistry.has(providerName)) {
+      return;
+    }
+
+    match(providerName)
+      .with('google', () => {
+        providerRegistry.register('google', new GoogleLLMProvider({ apiKey }));
+      })
+      .with('openai', () => {
+        providerRegistry.register('openai', new OpenAILLMProvider({ apiKey }));
+      })
+      .exhaustive();
+  });
 };
