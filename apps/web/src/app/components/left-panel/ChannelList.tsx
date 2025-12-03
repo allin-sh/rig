7c93d6ab6e2aa947145f6f2217212c286202ba19
gaki2 <qudrkr0815@gmail.com>
2025-12-04 00:06:59 +0900

    feat: hide empty channel & sort channel list by updatedAt

--- apps/web/src/app/components/left-panel/ChannelList.tsx
+++ apps/web/src/app/components/left-panel/ChannelList.tsx
@@ -1,4 +1,6 @@
+import { sortBy } from 'es-toolkit';
 import { useSetAtom } from 'jotai';
+import type z from 'zod';
 import {
   SidebarContent,
   SidebarGroup,
@@ -9,19 +11,38 @@ import {
   SidebarMenuItem,
 } from '@/components/ui/sidebar';
 import { useSwrAtomValue } from '@/hooks/use-swr-atom-value';
+import type { ChannelSchema } from '@/idb/db';
 import { dbAtoms } from '@/idb/db-store';
 import { assert } from '@/utils/assert';
 
+const formatDate = (timestamp: number): string => {
+  const date = new Date(timestamp);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+};
+
 export const ChannelList = () => {
   const allChannels = useSwrAtomValue(dbAtoms.allChannelsAtom);
-  const selectedChannelId = useSwrAtomValue(dbAtoms.selectedChannelIdAtom);
+  const selectedChannel = useSwrAtomValue(dbAtoms.selectedChannelAtom);
   const setSelectedChannelId = useSetAtom(dbAtoms.selectedChannelIdAtom);
 
   assert(allChannels, 'ChannelList: allChannels is not found.');
-  assert(selectedChannelId, 'ChannelList: selectedChannel is not found.');
+  assert(selectedChannel, 'ChannelList: selectedChannel is not found.');
 
   const onClick = (channelId: string) => {
+    if (selectedChannel.id === channelId) return;
+
     setSelectedChannelId(channelId);
+  };
+
+  const isGhostChannel = (channel: z.infer<typeof ChannelSchema>) => {
+    return (
+      channel.isEmpty && !channel.title && channel.id !== selectedChannel.id
+    );
   };
 
   return (
@@ -33,15 +54,19 @@ export const ChannelList = () => {
         <SidebarGroup>
           <SidebarGroupLabel>My Chats</SidebarGroupLabel>
           <SidebarGroupContent>
-            {allChannels.map(channel => {
+            {sortBy(
+              allChannels.filter(channel => !isGhostChannel(channel)),
+              [channel => -channel.updatedAt],
+            ).map(channel => {
               return (
                 <SidebarMenuItem key={channel.id}>
                   <SidebarMenuButton
-                    isActive={selectedChannelId === channel.id}
+                    isActive={selectedChannel.id === channel.id}
                     onClick={() => onClick(channel.id)}
                   >
                     <span className='truncate'>
-                      {channel.title ?? channel.id}
+                      {channel.title ??
+                        `Untitled ${formatDate(channel.createdAt)}`}
                     </span>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
