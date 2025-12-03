fd3e4b5c8163a189dff6392e65b76e9d339fe9a6
gaki2 <qudrkr0815@gmail.com>
2025-12-03 15:45:13 +0900

    feat: ChannelTitle Skeleton

---
+++ apps/web/src/app/components/header/ChannelTitle.tsx
@@ -0,0 +1,80 @@
+import { useSetAtom } from 'jotai';
+import { useCallback, useRef, useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { useSwrAtomValue } from '@/hooks/use-swr-atom-value';
+import { dbAtoms } from '@/idb/db-store';
+import { assert } from '@/utils/assert';
+
+const DEFAULT_NAME = 'Untitled';
+
+export const ChannelTitle = () => {
+  const selectedChannel = useSwrAtomValue(dbAtoms.selectedChannelAtom);
+  const updateSelectedChannel = useSetAtom(dbAtoms.selectedChannelAtom);
+  assert(selectedChannel, 'CenterHeader: selectedChannel is not found.');
+
+  const [isOpen, setIsOpen] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onSubmit = useCallback(() => {
+    const value = inputRef.current?.value;
+    if (!value) return;
+
+    updateSelectedChannel({
+      title: value,
+    });
+  }, [updateSelectedChannel]);
+
+  const onKeyDown = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter' && !(e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        onSubmit();
+        setIsOpen(false);
+      }
+    },
+    [onSubmit],
+  );
+
+  const hasChannelTitle = Boolean(selectedChannel.title);
+
+  return (
+    <Popover open={isOpen} onOpenChange={setIsOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant={'outline'}
+          size={'sm'}
+          data-has-channel-title={hasChannelTitle}
+          className='rounded-2xl backdrop-blur-md opacity-95 dark:aria-expanded:bg-input/80 dark:data-[has-channel-title=false]:text-muted-foreground/50'
+        >
+          {hasChannelTitle ? selectedChannel.title : DEFAULT_NAME}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        align='center'
+        side='bottom'
+        sideOffset={4}
+        className='w-92'
+        asChild
+      >
+        <Input
+          ref={inputRef}
+          onFocus={() => {
+            inputRef.current?.select();
+          }}
+          className='focus-visible:ring-0 rounded-2xl bg-accent dark:bg-card/90 backdrop-blur-xs'
+          placeholder='Chat title'
+          aria-selected={true}
+          defaultValue={selectedChannel.title ?? ''}
+          onBlur={onSubmit}
+          onKeyDown={onKeyDown}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
