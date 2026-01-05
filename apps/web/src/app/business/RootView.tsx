6d34d9947f6a03c5dcc4cedce5ce31bf932a6046
gaki2 <qudrkr0815@gmail.com>
2026-01-05 21:21:40 +0900

    feat: plugin floating ui

--- apps/web/src/app/business/RootView.tsx
+++ apps/web/src/app/business/RootView.tsx
@@ -5,11 +5,10 @@ import { Subject } from 'rxjs';
 import { ChatInput } from './chatting/ChatInput';
 import { Chatting } from './chatting/Chatting';
 import { ChattingSuspenseFallbackView } from './chatting/ChattingSuspenseFallbackView';
+import { ExtensionDock } from './dock/ExtensionDock';
 import { CenterHeader } from './header/CenterHeader';
-import { LeftHeader } from './header/LeftHeader';
 import { RightHeader } from './header/RightHeader';
 import { Initializer } from './Initializer';
-import { LeftPanelRenderer } from './left-panel/LeftPanelRenderer';
 
 export const RootViewRenderComponent$ = new Subject<
   Parameters<AUI['render']>[0] | null
@@ -33,6 +32,7 @@ export const RootView = React.memo(() => {
   return (
     <div className={'w-full h-full flex flex-row'}>
       <Initializer />
+      <ExtensionDock />
       <Suspense fallback={<div>Loading...</div>}>
         <RightHeader />
       </Suspense>
